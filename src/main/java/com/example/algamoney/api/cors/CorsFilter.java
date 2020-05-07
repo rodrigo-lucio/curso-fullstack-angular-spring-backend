66194e2fb8788ff7a94a06defdbbfc72acfba612
Rodrigo Lucio <luciodigo@gmail.com>
2020-05-06 23:20:37 -0300

    Testes de origem permitida durante o deploy no Heroku

--- src/main/java/com/example/algamoney/api/cors/CorsFilter.java
+++ src/main/java/com/example/algamoney/api/cors/CorsFilter.java
@@ -41,9 +41,9 @@ public class CorsFilter implements Filter{
 		
 		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
 	    response.setHeader("Access-Control-Allow-Credentials", "true");
-	    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+	    response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
 	    response.setHeader("Access-Control-Max-Age", "3600");
-	    response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, X-Requested-With, remember-me");
+	    response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
 	    response.setStatus(HttpServletResponse.SC_OK);
 	    chain.doFilter(req, res);
 	    
