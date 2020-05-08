71612739f97afd16572dde32f09f3f2a4aba5629
Rodrigo Lucio <luciodigo@gmail.com>
2020-05-07 22:07:19 -0300

    Testes de origem permitida durante o deploy no Heroku

--- src/main/java/com/example/algamoney/api/cors/CorsFilter.java
+++ src/main/java/com/example/algamoney/api/cors/CorsFilter.java
@@ -14,12 +14,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.example.algamoney.api.config.property.AlgamoneyApiProperty;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class CorsFilter implements Filter{
+public class CorsFilter implements Filter,WebMvcConfigurer{
 	
 	/*
 	 * Classe para fazer o filtro de requisição do CORS,
@@ -31,25 +33,24 @@ public class CorsFilter implements Filter{
 	@Autowired
 	private AlgamoneyApiProperty algamoneyProperty;
 
-
+	
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+	
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
 		
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;
-
-//Este trecho ok para heroku		
-//		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-//	    response.setHeader("Access-Control-Allow-Credentials", "true");
-//	    response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
-//	    response.setHeader("Access-Control-Max-Age", "3600");
-//	    response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
-//	    response.setStatus(HttpServletResponse.SC_OK);
-//	    chain.doFilter(req, res);
+		/*
+		
 	    
 //		response.setHeader("Access-Control-Allow-Origin", algamoneyProperty.getOriginPermitida());					//Esses dois estao fora pq serão sempre enviados, em todas as requisições, para que um post ou get depois funcionem. Ou seja, para que a aplicação continue funcionando após configurar o CORS
-		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+//		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Credentials", "true");												//Para o cookie ser enviado				
 
 		
@@ -68,6 +69,33 @@ public class CorsFilter implements Filter{
 			chain.doFilter(req, res);
 		}
 		
+		*/
+		
+		  response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+	      response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+	      response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,observe");
+	      response.setHeader("Access-Control-Max-Age", "3600");
+	      response.setHeader("Access-Control-Allow-Credentials", "true");
+	      response.setHeader("Access-Control-Expose-Headers", "Authorization");
+	      response.addHeader("Access-Control-Expose-Headers", "responseType");
+	      response.addHeader("Access-Control-Expose-Headers", "observe");
+	      System.out.println("Request Method: "+request.getMethod());
+	      if (!(request.getMethod().equalsIgnoreCase("OPTIONS"))) {
+	          try {
+	              chain.doFilter(req, res);
+	          } catch(Exception e) {
+	              e.printStackTrace();
+	          }
+	      } else {
+	          System.out.println("Pre-flight");
+	          response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+	          response.setHeader("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT");
+	          response.setHeader("Access-Control-Max-Age", "3600");
+	          response.setHeader("Access-Control-Allow-Headers", "Access-Control-Expose-Headers"+"Authorization, content-type," +
+	          "USERID"+"ROLE"+
+	                  "access-control-request-headers,access-control-request-method,accept,origin,authorization,x-requested-with,responseType,observe");
+	          response.setStatus(HttpServletResponse.SC_OK);
+	      }		
 		
 	}
 
